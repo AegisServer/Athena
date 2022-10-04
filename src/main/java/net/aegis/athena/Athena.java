6244bcad7e308fe3d81f0ed615769d63e9bd6184
Cynshiii <cynthiaemleigh@gmail.com>
2022-10-04 16:48:44 -0500

    add kyori bukkit adventure api

--- src/main/java/net/aegis/athena/Athena.java
+++ src/main/java/net/aegis/athena/Athena.java
@@ -3,29 +3,52 @@ package net.aegis.athena;
 import net.aegis.athena.features.commands.DiscordCommand;
 import net.aegis.athena.features.listeners.JoinLeaveMessages;
 import net.aegis.athena.features.listeners.RandomSpawn;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public final class Athena extends JavaPlugin {
+
+	private BukkitAudiences adventure;
+
+	public @NonNull BukkitAudiences adventure() {
+		if(this.adventure == null) {
+			throw new IllegalStateException("Tried to access Adventure when the plugin was disabled!");
+		}
+		return this.adventure;
+	}
 
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
 
 		//register listener classes
-
 		getServer().getPluginManager().registerEvents(new JoinLeaveMessages(), this);
 		getServer().getPluginManager().registerEvents(new RandomSpawn(), this);
-
 		//end of listener registry
 
 		//register command classes
+		getCommand("discord").setExecutor(new DiscordCommand(this));
+		//end of command registry
 
-		getCommand("discord").setExecutor((new DiscordCommand()));
+		//kyori adventure registry
+		this.adventure = BukkitAudiences.create(this);
+		//end of kyori adventure registry
 
 	}
 
 	@Override
 	public void onDisable() {
 		// Plugin shutdown logic
+
+		//kyori adventure shutdown logic
+
+		if(this.adventure != null) {
+			this.adventure.close();
+			this.adventure = null;
+		}
+
+		//end of kyori adventure
+
 	}
 }
