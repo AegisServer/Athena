e95b9477dc5d780c9c19eb0e4cd11c0443b1c5bb
Cynshiii <cynthiaemleigh@gmail.com>
2022-10-05 19:43:53 -0500

    add json builder instead of using adventureapi

--- src/main/java/net/aegis/athena/features/commands/DiscordCommand.java
+++ src/main/java/net/aegis/athena/features/commands/DiscordCommand.java
@@ -1,18 +1,14 @@
 package net.aegis.athena.features.commands;
 
 import net.aegis.athena.Athena;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.format.TextColor;
+import net.aegis.athena.utils.JsonBuilder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class DiscordCommand extends net.aegis.athena.utils.ChatColor implements CommandExecutor {
+public class DiscordCommand implements CommandExecutor {
 
 	private final Athena athena;
 
@@ -26,14 +22,17 @@ public class DiscordCommand extends net.aegis.athena.utils.ChatColor implements 
 
 		if (!(sender instanceof Player player))
 			return true;
-		String aegisRed = "#9F6E75";
-		String aegisBlue = "#6E759F";
 
-		TextComponent component = Component.text("Click here to join our Discord!", TextColor.fromHexString(aegisRed))
-				.hoverEvent(HoverEvent.showText(Component.text("Click here to open an invite in your browser", TextColor.fromHexString(aegisBlue))))
-				.clickEvent(ClickEvent.openUrl("https://discord.gg/pUZq9v7uJA"));
-		athena.adventure().player((Player) sender).sendMessage(component);
+//		TextComponent component = Component.text("Click here to join our Discord!", TextColor.fromHexString(aegisRed))
+//				.hoverEvent(HoverEvent.showText(Component.text("Click here to open an invite in your browser", TextColor.fromHexString(aegisBlue))))
+//				.clickEvent(ClickEvent.openUrl("https://discord.gg/pUZq9v7uJA"));
+//		athena.adventure().player((Player) sender).sendMessage(component);
 
+		new JsonBuilder()
+				.next("&#9F6E75Click here to join our Discord!")
+				.hover("&#6E759FClick here to open an invite in your browser")
+				.url("https://discord.gg/pUZq9v7uJA")
+				.send(player);
 		return true;
 	}
 
