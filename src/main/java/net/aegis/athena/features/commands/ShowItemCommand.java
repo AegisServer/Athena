382edaf7999a8395a443bdbd4b5d9b2925f29a11
Cynshiii <cynthiaemleigh@gmail.com>
2022-10-08 12:35:22 -0500

    add comments

--- src/main/java/net/aegis/athena/features/commands/ShowItemCommand.java
+++ src/main/java/net/aegis/athena/features/commands/ShowItemCommand.java
@@ -19,24 +19,28 @@ public class ShowItemCommand implements CommandExecutor {
 		if (!(player.hasPermission("athena.showitem")))
 			return true;
 
-		//get player item in hand and put in variable to use in new itemstack
+		//get player item in hand and put in variable to use later
 		ItemStack item = ItemUtils.getTool(player);
 
 		if (Nullables.isNullOrAir(item)){
 			PlayerUtils.send(player, "&#9F6E75You are not holding anything");
 			return true;
+			// if player item == null or air, end code and send player error message)
 		}
 
 		String itemName = item.getItemMeta().getDisplayName();
 		String material = StringUtils.camelCase(item.getType().name());
+		// set variable for ItemMeta display name and material name to turn into camel case instead of upper case
 
 		if (Nullables.isNullOrEmpty(itemName))
 			itemName = material;
+		// if itemname does not exist (vanilla items), set item name to the material
 
 		new JsonBuilder()
 				.next("&8&l[&r" + itemName + "&8&l]&r")
 				.hover(item)
 				.send(player);
+		//create jsonbuilder to send player their item in chat with a hover for that item (shows name,lore,etc)
 
 		return true;
 	}
