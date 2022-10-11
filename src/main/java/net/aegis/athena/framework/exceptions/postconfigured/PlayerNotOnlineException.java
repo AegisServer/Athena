b5caf881fb7c969dfd479d6946e940074d689336
Cynshiii <cynthiaemleigh@gmail.com>
2022-10-11 00:37:12 -0500

    add PlayerNotOnlineException

--- src/main/java/net/aegis/athena/framework/exceptions/postconfigured/PlayerNotOnlineException.java
+++ src/main/java/net/aegis/athena/framework/exceptions/postconfigured/PlayerNotOnlineException.java
@@ -1,13 +1,20 @@
 package net.aegis.athena.framework.exceptions.postconfigured;
 
+import net.aegis.athena.framework.interfaces.HasUniqueId;
+import net.aegis.athena.framework.persistence.mongodb.models.nickname.Nickname;
 import net.aegis.athena.utils.PlayerUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 public class PlayerNotOnlineException extends PostConfiguredException {
 
 	public PlayerNotOnlineException(UUID uuid) {
-		super(PlayerUtils.getPlayer(uuid).getName() + " is not online");
+		super(Nickname.of(uuid) + " is not online");
+	}
+
+	public PlayerNotOnlineException(HasUniqueId player) {
+		super(Nickname.of(player) + " is not online");
 	}
 
 }
