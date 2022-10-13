87649d350524422eb028c9a4eb11d465cfb76b61
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2022-10-13 02:12:29 -0500

    ShowItem: support slot argument, default to main hand

--- src/main/java/net/aegis/athena/features/commands/ShowItemCommand.java
+++ src/main/java/net/aegis/athena/features/commands/ShowItemCommand.java
@@ -2,19 +2,22 @@ package net.aegis.athena.features.commands;
 
 import lombok.NonNull;
 import net.aegis.athena.framework.commands.models.CustomCommand;
+import net.aegis.athena.framework.commands.models.annotations.Arg;
+import net.aegis.athena.framework.commands.models.annotations.Cooldown;
 import net.aegis.athena.framework.commands.models.annotations.Path;
 import net.aegis.athena.framework.commands.models.annotations.Permission;
+import net.aegis.athena.framework.commands.models.annotations.Permission.Group;
 import net.aegis.athena.framework.commands.models.events.CommandEvent;
 import net.aegis.athena.utils.Nullables;
 import net.aegis.athena.utils.StringUtils;
+import net.aegis.athena.utils.TimeUtils.TickTime;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
-/*
- * TODO:
- * 	- turn .send(player) into a broadcast being sent from the player so that other users can see
- * 		the /showitem; requires own chat api let users specify between hand or offhand- armor slots
- * 		will not be supported
- */
+import static net.aegis.athena.utils.Nullables.isNullOrAir;
+
+// TODO: turn into a message being sent from the player, so that other users can see the /showitem
 
 @Permission("athena.showitem")
 public class ShowItemCommand extends CustomCommand {
@@ -23,10 +26,11 @@ public class ShowItemCommand extends CustomCommand {
 		super(event);
 	}
 
-	@Path
-	void run() {
+	@Path("[hand|offhand]")
+	@Cooldown(value = TickTime.SECOND, x = 15, bypass = Group.ADMIN)
+	void run(@Arg("hand") String slot) {
 		//get player item in hand and put in variable to use later
-		ItemStack item = getToolRequired();
+		ItemStack item = getItem(player(), slot);
 
 		// set variable for ItemMeta display name and material name to turn into camel case instead of upper case
 		String itemName = item.getItemMeta().getDisplayName();
@@ -38,5 +42,25 @@ public class ShowItemCommand extends CustomCommand {
 
 		//create jsonbuilder to send player their item in chat with a hover for that item (shows name,lore,etc)
 		send(json().next("&8&l[&r" + itemName + "&8&l]&r").hover(item));
+	}
+
+	private ItemStack getItem(Player player, String slot) {
+		ItemStack item = null;
+		PlayerInventory inv = player.getInventory();
+
+		switch (slot.toLowerCase()) {
+			case "offhand" -> item = inv.getItemInOffHand();
+			case "mainhand", "hand" -> item = inv.getItemInMainHand();
+			case "hat", "head", "helm", "helmet" -> item = inv.getHelmet();
+			case "chest", "chestplate" -> item = inv.getChestplate();
+			case "pants", "legs", "leggings" -> item = inv.getLeggings();
+			case "boots", "feet", "shoes" -> item = inv.getBoots();
+			default -> error("Unknown slot &e" + slot);
+		}
+
+		if (isNullOrAir(item))
+			error("Item in " + slot + " not found");
+
+		return item;
 	}
 }
--- src/main/java/net/aegis/athena/framework/commands/models/annotations/Cooldown.java
+++ src/main/java/net/aegis/athena/framework/commands/models/annotations/Cooldown.java
@@ -7,12 +7,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Deprecated // TODO CYN: setup cooldown service
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Cooldown {
 	TickTime value();
+
 	double x() default 1;
+
 	boolean global() default false;
+
 	String bypass() default "";
 
 }
