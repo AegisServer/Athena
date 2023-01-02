5a1be460fd747b3f48b86bdff3f0f4660bd45845
Cynshiii <cynthiaemleigh@gmail.com>
2023-01-02 00:03:10 -0600

    start armor stand editor; disabled until finished

---
+++ src/main/java/net/aegis/athena/features/commands/ArmorStandEditorCommand.java
@@ -0,0 +1,17 @@
+package net.aegis.athena.features.commands;
+
+//@Disabled //TODO enable when fixed
+//@Aliases({"ase", "armourstandeditor"})
+//public class ArmorStandEditorCommand extends CustomCommand {
+//
+//	public ArmorStandEditorCommand(@NonNull CommandEvent event) {
+//		super(event);
+//	}
+//
+//	@Path("arms [enable]")
+//	void arms(Boolean enabled) {
+//		ArmorStand armorStand = (ArmorStand) getTargetEntityRequired(EntityType.ARMOR_STAND);
+//
+//		if (!isPerkAllowedAt(player()))
+//	}
+//}
